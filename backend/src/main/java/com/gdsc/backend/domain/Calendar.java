251397fcd5cea3103d789b3cd0e43046959791e2
WannaBeTop <optimuslove0223@icloud.com>
2024-05-25 00:38:27 +0900

    Feat : 모든 Entity 구현완료

---
+++ backend/src/main/java/com/gdsc/backend/domain/Calendar.java
@@ -0,0 +1,23 @@
+package com.gdsc.backend.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Calendar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CalendarID", updatable = false)
+    private Long CalendarID;
+    @ManyToOne
+    @JoinColumn(name = "UserID")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "CertificationID")
+    private Certification certification;
+
+    @ManyToOne
+    @JoinColumn(name = "ScheduleID")
+    private Schedule schedule;
+}
